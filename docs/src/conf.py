e2c7b8d449ed04386cdfa5f2e231d27108b27877
Lynn Root <lynn@spotify.com>
2020-08-18 13:06:55 -0400

    [docs] Update copyright and format w black

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'klio'
-copyright = '2020, The klio developers'
-author = 'The klio developers'
+project = "klio"
+copyright = "2020, Spotify AB"
+author = "The klio developers"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +31,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +44,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]

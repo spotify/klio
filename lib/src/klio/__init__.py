172178328d393461a972677ba5536a636f26ddda
Dan Simon <dsimon@spotify.com>
2020-08-19 12:09:31 -0400

    [lib] Bump version: 0.2.0.dev0 → 0.2.1.dev1

--- lib/src/klio/__init__.py
+++ lib/src/klio/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Spotify AB
 #
 __author__ = "The klio developers"
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.1.dev1"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Conventions for Python + Dataflow "
 __uri__ = "https://github.com/spotify/klio"

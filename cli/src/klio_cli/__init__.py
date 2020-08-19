0a99795b82b87aeb64b3d0820af28230a143df5d
Dan Simon <dsimon@spotify.com>
2020-08-19 13:12:30 -0400

    [cli] Bump version: 1.0.0.dev0 → 1.0.0.dev1

--- cli/src/klio_cli/__init__.py
+++ cli/src/klio_cli/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Spotify AB
 
 __author__ = "The klio developers"
-__version__ = "1.0.0.dev0"
+__version__ = "1.0.0.dev1"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "todo"
 __uri__ = "https://github.com/spotify/klio"

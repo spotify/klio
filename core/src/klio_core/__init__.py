0c30ebbd5f27d56442ed5ee1096a88f48b22bdff
Lynn Root <lynn@spotify.com>
2020-09-02 09:52:06 -0400

    [core] Prep for first alpha release

--- core/src/klio_core/__init__.py
+++ core/src/klio_core/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Spotify AB
 
 __author__ = "The klio developers"
-__version__ = "0.2.0.dev4"
+__version__ = "0.2.0.alpha0"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Core klio library for common functionality"
 __uri__ = "https://github.com/spotify/klio"

ccc96bd2a3bcf3c8024afcbae866fbe4772248a7
Lynn Root <lynn@spotify.com>
2020-09-02 10:10:38 -0400

    [exec] Prep for first alpha release

--- exec/src/klio_exec/__init__.py
+++ exec/src/klio_exec/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Spotify AB
 #
 __author__ = "The klio developers"
-__version__ = "0.2.0.dev1"
+__version__ = "0.2.0.alpha0"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Component of klio project that reduces boilerplate while \
                    writing YAML-config-driven, Dockerized python Beam pipelines\

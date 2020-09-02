a8fae612735f48c1bcd462fb60a18a1f2f6dc553
Lynn Root <lynn@spotify.com>
2020-09-02 09:59:01 -0400

    [devtools] Bump version: 0.1.0.dev0 → 0.1.0

--- devtools/src/klio_devtools/__init__.py
+++ devtools/src/klio_devtools/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Spotify AB
 
 __author__ = "The klio developers"
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Utilities to make klio development a little easier."
 __uri__ = "https://github.com/spotify/klio"

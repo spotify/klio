0c5498416db5c89fca7288cbc62b394a481d19ea
Lynn Root <lynn@spotify.com>
2020-09-09 10:17:17 -0400

    [docs] Correct inherited docs for IO transforms

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -90,6 +90,37 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# sphinx-build has a "nitpick" mode (used during CI docs workflow and
+# `make stricthtml`). We inherit some docs from Apache Beam, and some of
+# their docstrings fail to compile in nitpick mode.
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-nitpick_ignore
+nitpick_ignore = [
+    # <-- nitpick docstrings from beam.io.gcp.WriteToBigQuery
+    ("py:class", "callable"),
+    ("py:class", "ValueProvider"),
+    ("py:class", "apache_beam.io.gcp.internal.clients.bigquery.bigquery_v2_messages.TableSchema"),
+    ("py:class", "BigQueryDisposition"),
+    ("py:attr", "BigQueryDisposition.CREATE_IF_NEEDED"),
+    ("py:attr", "BigQueryDisposition.CREATE_NEVER"),
+    ("py:attr", "BigQueryDisposition.WRITE_TRUNCATE"),
+    ("py:attr", "BigQueryDisposition.WRITE_APPEND"),
+    ("py:attr", "BigQueryDisposition.WRITE_EMPTY"),
+    # -->
+    # <-- nitpick docstrings from beam.io.textio.WriteToText
+    ("py:class", "WriteToText"),
+    ("py:class", "apache_beam.io.filesystem.CompressionTypes.AUTO"),
+    # -->
+    # <-- nitpick drom beam.io.ReadFromText
+    ("py:class", "ReadFromText"),
+    # -->
+]
+
+# sphinx-build -b linkcheck will error out if links in docstrings are broken,
+# including inherited docstrings (i.e. Beam)
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-linkcheck_ignore
+linkcheck_ignore = [
+    r"https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs#configuration.load",
+]
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -132,6 +163,10 @@ autodoc_default_options = {
 # Note: this won't work for Apache Beam because they generate the
 # python documentation per SDK version, and we probably shouldn't hard-link
 # to a specific version
+# Update ^: I emailed users@beam.apache.org and they're looking into it:
+# https://lists.apache.org/thread.html/r57a910d1689ace1c9067d2a702468325
+#         f6800626d82edef0a6d80961%40%3Cuser.beam.apache.org%3E
 intersphinx_mapping = {
     "https://docs.python.org/3": None,
+    "https://beam.apache.org/releases/pydoc/2.23.0/": None,
 }

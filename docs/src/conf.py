8c5737f14ce92927f1b632319be69f8905001717
Lynn Root <lynn@spotify.com>
2020-09-11 14:20:18 -0400

    [docs] Update color palette for accessibility

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -140,10 +140,10 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/custom.css"]  # relative to _static
+html_css_files = ["css/custom.css", "css/colors.css"]  # relative to _static
 html_js_files = ["js/custom.js"]  # relative to _static
 html_favicon = "_static/images/k_favicon.png"
-
+pygments_style = "vs"
 
 # -- Extention configuration
 

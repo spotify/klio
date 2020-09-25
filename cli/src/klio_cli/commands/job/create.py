4182c55679868108fc7f4bb17b148cd6079008d0
Fallon Chen <fallon@spotify.com>
2020-09-25 21:21:03 +0200

    [cli] job create - switch to using rendering module inside create and remove old rendering code

--- cli/src/klio_cli/commands/job/create.py
+++ cli/src/klio_cli/commands/job/create.py
@@ -13,17 +13,14 @@
 # limitations under the License.
 #
 
-import datetime
-import io
 import logging
-import os
 import re
 
 import click
 import emoji
-import jinja2
 
 from klio_cli.commands.job.utils import gcp_setup
+from klio_cli.commands.job.utils import rendering
 
 
 # Valid project IDs: https://cloud.google.com/resource-manager/reference
@@ -58,103 +55,6 @@ class CreateJob(object):
 
     BASE_TOPIC_TPL = "projects/{gcp_project}/topics/{job_name}"
     WORKER_IMAGE_TPL = "gcr.io/{gcp_project}/{job_name}-worker"
-
-    def _get_environment(self):
-        here = os.path.abspath(__file__)
-        here_dir = os.path.join(here, os.path.pardir)
-        here_dir = os.path.abspath(here_dir)
-        template_dir = os.path.join(here_dir, "utils", "templates")
-        return jinja2.Environment(
-            loader=jinja2.FileSystemLoader(template_dir), autoescape=False
-        )
-
-    # TODO: add a boolean flag to cli.create_job command to force-recreate
-    #       if the particular job directory already exists. This would
-    #       essentially overwrite all the klio-generated files. (@lynn)
-    def _create_job_directory(self, output_dir):
-        try:
-            os.mkdir(output_dir)
-        except OSError as e:
-            if e.errno != 17:  # file already exists
-                raise
-
-    def _write_template(self, output_dir, output_file, data):
-        output = os.path.abspath(os.path.join(output_dir, output_file))
-        # TODO: wrap the writing of a file in a `try`/`except`. If there is
-        #       an error in writing a file, raise a user-friendly exception.
-        #       This might leave the job-generation in a weird state (i.e.
-        #       some files created). We should decide what we want to do, i.e.
-        #       leave our partially-created mess there, or clean up after
-        #       ourselves. (@lynn)
-        with io.open(output, "w", encoding="utf-8") as f:
-            f.write(data)
-
-        logging.debug("Created {0}".format(output))
-
-    def _create_job_config(self, env, context, job_dir):
-        config_tpl = env.get_template("klio-job.yaml.tpl")
-
-        template_context = {"klio": context}
-        rendered_file = config_tpl.render(template_context)
-        self._write_template(job_dir, "klio-job.yaml", rendered_file)
-
-    def _create_python_files(self, env, package_name, output_dir):
-        current_year = datetime.datetime.now().year
-        template_context = {
-            "klio": {"year": current_year, "package_name": package_name}
-        }
-
-        init_tpl = env.get_template("init.py.tpl")
-        init_rendered = init_tpl.render(template_context)
-        self._write_template(output_dir, "__init__.py", init_rendered)
-
-        run_tpl = env.get_template("run.py.tpl")
-        run_rendered = run_tpl.render(template_context)
-        self._write_template(output_dir, "run.py", run_rendered)
-
-        transforms_tpl = env.get_template("transforms.py.tpl")
-        transforms_rendered = transforms_tpl.render(template_context)
-        self._write_template(output_dir, "transforms.py", transforms_rendered)
-
-        test_transforms_tpl = env.get_template("test_transforms.py.tpl")
-        test_transforms_rendered = test_transforms_tpl.render(template_context)
-        self._write_template(
-            output_dir, "test_transforms.py", test_transforms_rendered
-        )
-
-    def _create_no_fnapi_files(self, env, context, output_dir):
-        template_context = {"klio": context}
-
-        setup_tpl = env.get_template("setup.py.tpl")
-        setup_rendered = setup_tpl.render(template_context)
-        self._write_template(output_dir, "setup.py", setup_rendered)
-
-        manifest_tpl = env.get_template("MANIFEST.in.tpl")
-        manifest_rendered = manifest_tpl.render(template_context)
-        self._write_template(output_dir, "MANIFEST.in", manifest_rendered)
-
-    def _create_reqs_file(self, env, context, output_dir):
-        # the reqs file no longer has template context needed to be filled in,
-        # but leaving this here in case we do change our minds (i.e. explicitly
-        # including a klio package)
-        template_context = {"klio": context}
-        reqs_tpl = env.get_template("job-requirements.txt.tpl")
-        reqs_rendered = reqs_tpl.render(template_context)
-        self._write_template(output_dir, "job-requirements.txt", reqs_rendered)
-
-    def _create_dockerfile(self, env, context, output_dir):
-        template_context = {"klio": context}
-
-        dockerfile_tpl = env.get_template("dockerfile.tpl")
-        dockerfile_rendered = dockerfile_tpl.render(template_context)
-        self._write_template(output_dir, "Dockerfile", dockerfile_rendered)
-
-    def _create_readme(self, env, context, output_dir):
-        template_context = {"klio": context}
-
-        setup_inst_tpl = env.get_template("README.md.tpl")
-        setup_inst_rendered = setup_inst_tpl.render(template_context)
-        self._write_template(output_dir, "README.md", setup_inst_rendered)
 
     def _validate_worker_image(self, value):
         # TODO: add validation that the image string is in the format of
@@ -588,20 +488,21 @@ class CreateJob(object):
 
         self._create_external_resources(context)
 
-        env = self._get_environment()
+        template_renderer = rendering.CreateJobTemplateRenderer()
+        env = template_renderer.get_environment()
         job_name = context["job_name"]
         package_name = job_name.replace("-", "_")
-        self._create_job_directory(output_dir)
-        self._create_job_config(env, context, output_dir)
+        template_renderer.create_job_directory(output_dir)
+        template_renderer.create_job_config(env, context, output_dir)
 
-        self._create_python_files(env, package_name, output_dir)
+        template_renderer.create_python_files(env, package_name, output_dir)
         if not context["use_fnapi"]:
             context["package_name"] = package_name
-            self._create_no_fnapi_files(env, context, output_dir)
-        self._create_reqs_file(env, context, output_dir)
+            template_renderer.create_no_fnapi_files(env, context, output_dir)
+        template_renderer.create_reqs_file(env, context, output_dir)
         if create_dockerfile:
-            self._create_dockerfile(env, context, output_dir)
-        self._create_readme(env, context, output_dir)
+            template_renderer.create_dockerfile(env, context, output_dir)
+        template_renderer.create_readme(env, context, output_dir)
 
         msg = "Klio job {} created successfully! :beer:".format(job_name)
         logging.info(emoji.emojize(msg, use_aliases=True))
--- cli/tests/commands/job/test_create.py
+++ cli/tests/commands/job/test_create.py
@@ -13,16 +13,12 @@
 # limitations under the License.
 #
 
-import datetime
 import os
 
 import click
-import mock
 import pytest
-import yaml
 
 from klio_cli.commands.job import create
-
 
 HERE = os.path.abspath(os.path.join(os.path.abspath(__file__), os.path.pardir))
 FIXTURE_PATH = os.path.join(HERE, "utils", "fixtures")
@@ -31,70 +27,6 @@ FIXTURE_PATH = os.path.join(HERE, "utils", "fixtures")
 @pytest.fixture
 def job():
     return create.CreateJob()
-
-
-# pytest prevents monkeypatching datetime directly
-class MockDatetime(datetime.datetime):
-    @classmethod
-    def now(cls):
-        return datetime.datetime(2019, 1, 1)
-
-
-def test_get_environment(job):
-    env = job._get_environment()
-    expected_templates = [
-        "MANIFEST.in.tpl",
-        "README.md.tpl",
-        "dockerfile.tpl",
-        "init.py.tpl",
-        "job-requirements.txt.tpl",
-        "klio-job.yaml.tpl",
-        "run.py.tpl",
-        "setup.py.tpl",
-        "test_transforms.py.tpl",
-        "transforms.py.tpl",
-    ]
-    assert expected_templates == sorted(env.list_templates())
-
-
-def test_create_job_directory(tmpdir, job):
-    output_base_dir = tmpdir.mkdir("testing")
-    output_dir = os.path.join(str(output_base_dir), "test_job")
-    job._create_job_directory(output_dir)
-
-    assert os.path.exists(output_dir)
-
-
-@pytest.mark.parametrize("error_code", (1, 17))
-def test_create_job_directory_raises(monkeypatch, tmpdir, error_code, job):
-    output_base_dir = tmpdir.mkdir("testing")
-    output_dir = os.path.join(str(output_base_dir), "test_job")
-
-    def mock_mkdir(*args, **kwargs):
-        raise OSError(error_code, "some error message", output_dir)
-
-    monkeypatch.setattr(create.os, "mkdir", mock_mkdir)
-
-    if error_code != 17:
-        with pytest.raises(OSError) as e:
-            job._create_job_directory(output_dir)
-
-        assert e.match("some error message")
-    else:
-        # should not re-raise if file already existsgit
-        job._create_job_directory(output_dir)
-
-
-def test_write_template(tmpdir, job):
-    data = u"this is some test data"
-    output_dir = tmpdir.mkdir("testing")
-    output_file = "foo.txt"
-
-    job._write_template(output_dir.strpath, output_file, data)
-
-    ret_file = output_dir.join(output_file)
-    ret_contents = ret_file.read()
-    assert data == ret_contents
 
 
 @pytest.fixture
@@ -186,162 +118,6 @@ def default_context():
             ],
         },
     }
-
-
-@pytest.mark.parametrize("use_fnapi", (True, False))
-def test_create_job_config(use_fnapi, context, tmpdir, monkeypatch, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("jobs").mkdir("test_job")
-    env = job._get_environment()
-
-    context["use_fnapi"] = use_fnapi
-    if not use_fnapi:
-        monkeypatch.setitem(context["pipeline_options"], "experiments", [])
-    job._create_job_config(env, context, output_dir.strpath)
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected")
-    is_fnapi_dir = "fnapi" if use_fnapi else "no_fnapi"
-    expected_fixtures = os.path.join(expected_fixtures, is_fnapi_dir)
-
-    fixture = os.path.join(expected_fixtures, "klio-job.yaml")
-    with open(fixture, "r") as f:
-        expected = yaml.safe_load(f)
-
-    ret_file = output_dir.join("klio-job.yaml")
-    ret_contents = yaml.safe_load(ret_file.read())
-
-    assert expected == ret_contents
-
-
-def test_create_python_files(tmpdir, mocker, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("jobs").mkdir("test_job")
-    env = job._get_environment()
-
-    dt_patch = "klio_cli.commands.job.create.datetime.datetime"
-    with mock.patch(dt_patch, MockDatetime):
-        job._create_python_files(env, "test_job", output_dir.strpath)
-
-    ret_init_file = output_dir.join("__init__.py")
-    ret_init_contents = ret_init_file.read()
-    ret_run_file = output_dir.join("run.py")
-    ret_run_contents = ret_run_file.read()
-    ret_transforms_file = output_dir.join("transforms.py")
-    ret_transforms_contents = ret_transforms_file.read()
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected")
-    init_fixture = os.path.join(expected_fixtures, "__init__.py")
-    run_fixture = os.path.join(expected_fixtures, "run.py")
-    transforms_fixture = os.path.join(expected_fixtures, "transforms.py")
-    with open(init_fixture, "r") as f:
-        expected_init = f.read()
-
-    with open(run_fixture, "r") as f:
-        expected_run = f.read()
-
-    with open(transforms_fixture, "r") as f:
-        expected_transforms = f.read()
-
-    assert expected_init == ret_init_contents + "\n"
-    assert expected_run == ret_run_contents + "\n"
-    assert expected_transforms == ret_transforms_contents + "\n"
-
-
-def test_create_no_fnapi_files(tmpdir, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("jobs").mkdir("test_job")
-    env = job._get_environment()
-    context = {"use_fnapi": False, "package_name": "test-job"}
-
-    job._create_no_fnapi_files(env, context, output_dir.strpath)
-
-    manifest_file = output_dir.join("MANIFEST.in")
-    ret_manifest_contents = manifest_file.read()
-    setup_file = output_dir.join("setup.py")
-    ret_setup_contents = setup_file.read()
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected", "no_fnapi")
-
-    manifest_fixture = os.path.join(expected_fixtures, "MANIFEST.in")
-    setup_fixture = os.path.join(expected_fixtures, "setup.py")
-    with open(manifest_fixture, "r") as f:
-        expected_manifest = f.read()
-
-    with open(setup_fixture, "r") as f:
-        expected_setup = f.read()
-
-    assert expected_manifest == ret_manifest_contents + "\n"
-    assert expected_setup == ret_setup_contents + "\n"
-
-
-@pytest.mark.parametrize("use_fnapi", (True, False))
-def test_create_reqs_file(use_fnapi, tmpdir, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("jobs").mkdir("test_job")
-    env = job._get_environment()
-    context = {"use_fnapi": use_fnapi}
-
-    job._create_reqs_file(env, context, output_dir.strpath)
-
-    ret_reqs_file = output_dir.join("job-requirements.txt")
-    ret_reqs_contents = ret_reqs_file.read()
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected")
-    is_fnapi_dir = "fnapi" if use_fnapi else "no_fnapi"
-    expected_fixtures = os.path.join(expected_fixtures, is_fnapi_dir)
-
-    fixture = os.path.join(expected_fixtures, "job-requirements.txt")
-    with open(fixture, "r") as f:
-        expected = f.read()
-
-    assert expected == ret_reqs_contents + "\n"
-
-
-@pytest.mark.parametrize("use_fnapi", (True, False))
-def test_create_dockerfile(use_fnapi, tmpdir, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("jobs").mkdir("test_job")
-    env = job._get_environment()
-    context = {
-        "pipeline_options": {
-            "worker_harness_container_image": "gcr.io/foo/bar",
-            "project": "test-gcp-project",
-        },
-        "python_version": "36",
-        "use_fnapi": use_fnapi,
-        "create_resources": False,
-    }
-
-    job._create_dockerfile(env, context, output_dir.strpath)
-
-    ret_dockerfile_file = output_dir.join("Dockerfile")
-    ret_dockerfile_contents = ret_dockerfile_file.read()
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected")
-    is_fnapi_dir = "fnapi" if use_fnapi else "no_fnapi"
-    expected_fixtures = os.path.join(expected_fixtures, is_fnapi_dir)
-
-    fixture = os.path.join(expected_fixtures, "Dockerfile")
-    with open(fixture, "r") as f:
-        expected = f.read()
-
-    assert expected == ret_dockerfile_contents
-
-
-@pytest.mark.parametrize("use_fnapi", (True, False))
-def test_create_readme(use_fnapi, tmpdir, job):
-    output_dir = tmpdir.mkdir("testing").mkdir("test_job")
-    env = job._get_environment()
-    context = {"job_name": "test-job", "use_fnapi": use_fnapi}
-
-    job._create_readme(env, context, output_dir.strpath)
-
-    expected_fixtures = os.path.join(FIXTURE_PATH, "expected")
-    is_fnapi_dir = "fnapi" if use_fnapi else "no_fnapi"
-    expected_fixtures = os.path.join(expected_fixtures, is_fnapi_dir)
-
-    ret_file = output_dir.join("README.md")
-    ret_file_contents = ret_file.read()
-    exp_file = os.path.join(expected_fixtures, "README.md")
-    with open(exp_file, "r") as f:
-        exp_file_contents = f.read()
-
-    assert exp_file_contents == ret_file_contents
 
 
 def test_validate_worker_image(job):
@@ -554,11 +330,7 @@ def test_get_context_from_user_inputs(
 
 
 def test_get_context_from_user_inputs_no_prompts(
-    mocker,
-    context_overrides,
-    expected_overrides,
-    mock_prompt,
-    job,
+    mocker, context_overrides, expected_overrides, mock_prompt, job,
 ):
     context_overrides["machine_type"] = "n4-highmem-l33t"
     mock_validate_region = mocker.patch.object(job, "_validate_region")
@@ -579,11 +351,7 @@ def test_get_context_from_user_inputs_no_prompts(
 
 
 def test_get_context_from_user_inputs_no_prompts_image(
-    mocker,
-    context_overrides,
-    expected_overrides,
-    mock_prompt,
-    job,
+    mocker, context_overrides, expected_overrides, mock_prompt, job,
 ):
     mock_prompt.side_effect = [""]
 
@@ -665,17 +433,35 @@ def test_create(
 
     mock_get_user_input = mocker.patch.object(job, "_get_user_input")
     mock_get_user_input.return_value = (context, create_dockerfile)
-    mock_get_environment = mocker.patch.object(job, "_get_environment")
-    mock_create_job_dir = mocker.patch.object(job, "_create_job_directory")
-    mock_create_job_config = mocker.patch.object(job, "_create_job_config")
-    mock_create_no_fnapi_files = mocker.patch.object(
-        job, "_create_no_fnapi_files"
+    template_renderer = mocker.Mock()
+    template_renderer_const = mocker.patch.object(
+        create.rendering, "CreateJobTemplateRenderer"
     )
-    mock_create_python_files = mocker.patch.object(job, "_create_python_files")
-    mock_create_reqs_files = mocker.patch.object(job, "_create_reqs_file")
-    mock_create_dockerfile = mocker.patch.object(job, "_create_dockerfile")
-    mock_create_readme = mocker.patch.object(job, "_create_readme")
-
+    template_renderer_const.return_value = template_renderer
+    mock_get_environment = mocker.patch.object(
+        template_renderer, "get_environment"
+    )
+    mock_create_job_dir = mocker.patch.object(
+        template_renderer, "create_job_directory"
+    )
+    mock_create_job_config = mocker.patch.object(
+        template_renderer, "create_job_config"
+    )
+    mock_create_no_fnapi_files = mocker.patch.object(
+        template_renderer, "create_no_fnapi_files"
+    )
+    mock_create_python_files = mocker.patch.object(
+        template_renderer, "create_python_files"
+    )
+    mock_create_reqs_files = mocker.patch.object(
+        template_renderer, "create_reqs_file"
+    )
+    mock_create_dockerfile = mocker.patch.object(
+        template_renderer, "create_dockerfile"
+    )
+    mock_create_readme = mocker.patch.object(
+        template_renderer, "create_readme"
+    )
     mock_create_topics = mocker.patch.object(
         create.gcp_setup, "create_topics_and_buckets"
     )

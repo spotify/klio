4e6fcabfb34b881cd625e4273c97933dd10df22a
Lynn Root <lynn@spotify.com>
2020-10-01 12:59:49 -0400

    [cli] Add package description

--- cli/src/klio_cli/__init__.py
+++ cli/src/klio_cli/__init__.py
@@ -16,5 +16,5 @@
 __author__ = "The klio developers"
 __version__ = "1.0.0.alpha1"
 __email__ = "opensource+klio@spotify.com"
-__description__ = "todo"
+__description__ = "Main entrypoint for Klio jobs"
 __uri__ = "https://github.com/spotify/klio"

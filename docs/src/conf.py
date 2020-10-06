d1a4a1fdf122884acca453a8e70c543e78949e9a
Lynn Root <lynn@spotify.com>
2020-10-06 15:34:53 -0400

    [docs] Add fonts

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -176,7 +176,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/custom.css", "css/colors.css", "css/roles.css"]  # relative to _static
+html_css_files = ["css/fonts.css", "css/custom.css", "css/colors.css", "css/roles.css"]  # relative to _static
 html_js_files = ["js/custom.js"]  # relative to _static
 html_favicon = "_static/images/favicon.png"
 pygments_style = "vs"

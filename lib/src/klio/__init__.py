ea5e1184ea659919d4ca4fb79460cc2a0f28aaf9
Dan Simon <dsimon@spotify.com>
2021-02-22 09:43:30 -0500

    [lib] Bump version: 0.2.5rc1 → 0.2.5rc2

--- lib/src/klio/__init__.py
+++ lib/src/klio/__init__.py
@@ -14,7 +14,7 @@
 #
 
 __author__ = "The klio developers"
-__version__ = "0.2.5rc1"
+__version__ = "0.2.5rc2"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Conventions for Python + Apache Beam "
 __uri__ = "https://github.com/spotify/klio"

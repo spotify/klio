4f7a5700799def41cf389ca85484f44253bce791
Dan Simon <dsimon@spotify.com>
2021-07-29 10:54:53 -0400

    [core] Bump version: 21.7.0.dev2 → 21.7.0.dev3

--- core/src/klio_core/__init__.py
+++ core/src/klio_core/__init__.py
@@ -14,7 +14,7 @@
 #
 
 __author__ = "The klio developers"
-__version__ = "21.7.0.dev2"
+__version__ = "21.7.0.dev3"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Core klio library for common functionality"
 __uri__ = "https://github.com/spotify/klio"

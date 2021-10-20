78db4e9ab35e5d504c18a4d6bbbdfbdb86662d35
Lynn Root <lynn@spotify.com>
2021-10-20 10:38:13 -0400

    [exec] Bump version: 21.10.0.dev1 → 21.10.0.dev2

--- exec/src/klio_exec/__init__.py
+++ exec/src/klio_exec/__init__.py
@@ -14,7 +14,7 @@
 #
 
 __author__ = "The klio developers"
-__version__ = "21.10.0.dev1"
+__version__ = "21.10.0.dev2"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Klio pipeline executor within a job's Docker image."
 __uri__ = "https://github.com/spotify/klio"

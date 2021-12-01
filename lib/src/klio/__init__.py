7d81e12a768950e9761b3b0f6376c40eb5cd275b
Shireen Kheradpey <shireenkheradpey@gmail.com>
2021-12-01 14:35:36 -0500

    [lib] Bumpversion 21.9.0 -> 21.12.0rc1

--- lib/src/klio/__init__.py
+++ lib/src/klio/__init__.py
@@ -14,7 +14,7 @@
 #
 
 __author__ = "The klio developers"
-__version__ = "21.10.0.dev2"
+__version__ = "21.12.0rc1"
 __email__ = "opensource+klio@spotify.com"
 __description__ = "Conventions for Python + Apache Beam "
 __uri__ = "https://github.com/spotify/klio"
